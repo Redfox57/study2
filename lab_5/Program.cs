01d25a97929dc48128eb6b16321fb3c284158d5b
RedFox <novikov2090@yandex.ru>
2022-12-05 02:32:49 +0300

    lab_5 and lab_7

---
+++ lab_5/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Please enter 3 digit number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int ShowMidNum(int number)
+{
+    int remain = number % 100;
+    return remain / 10;
+}
+
+Console.WriteLine(ShowMidNum(num));
---
+++ lab_7/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Write("Please enter the number from 1 to 7: ");
+int day = Convert.ToInt32(Console.ReadLine());
+if (day >= 1 && day <= 7)
+{
+    if (day == 6 || day == 7)
+    {
+        Console.WriteLine("Yes");
+    } 
+    else
+    {
+        Console.WriteLine("No");
+    }
+}
+else
+{
+    Console.WriteLine("Number is out of the range!!!");
+}
