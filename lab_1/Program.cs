cb7fc27a8b19e657f80ea4dae7e98af295cb56c7
RedFox <novikov2090@yandex.ru>
2022-12-05 01:36:14 +0300

    lab_1 and lab_2

---
+++ lab_1/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Введите первое число:");
+string a =  Console.ReadLine(); 
+Console.WriteLine("Введите второе число:");
+string b =  Console.ReadLine(); 
+int numberOne = Convert.ToInt32(a);
+int numberTwo = Convert.ToInt32(b);
+
+if (numberOne == numberTwo * numberTwo)
+{
+    
+}
+int result = number * number;
+
+Console.WriteLine($"Квадрат числа {number} равен = {result}");
---
+++ lab_2/Program.cs
@@ -0,0 +1,18 @@
+Console.WriteLine("Введите число от 1 до 7: ");
+string dey = Console.ReadLine() ?? "";
+if (dey == "1")
+{
+    Console.WriteLine("Понедельник");
+}
+else if (dey == "2")
+{
+    Console.WriteLine("Вторник");
+}
+else if (dey == "3")
+{
+    Console.WriteLine("Среда");
+}
+else if (dey == "4")
+{
+    Console.WriteLine("Четверг");
+}
