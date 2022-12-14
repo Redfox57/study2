1787feb34e5739edf215a63934009d168157705b
RedFox <novikov2090@yandex.ru>
2022-12-14 19:16:12 +0300

    lab_19 to lab_23

---
+++ lab_19/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Write("Enter the 5 digit number: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int n0 = n / 10000;
+int n1 = n / 1000 % 10;
+int n2 = n / 100 % 10;
+int n3 = n / 10 % 10;
+int n4 = n % 10;
+
+if (n0 == n4 && n1 == n3) 
+{
+    Console.WriteLine($"Number '{n}' is Palindrome");
+}
+else 
+{
+    Console.WriteLine($"Number '{n}' is NO Palindrome");
+}
--- lab_5/Program.cs
+++ lab_5/Program.cs
@@ -1,7 +1,13 @@
 ﻿Console.Write("Please enter 3 digit number: ");
 int num = Convert.ToInt32(Console.ReadLine());
+int amount = num.ToString().Length;
 
-int ShowMidNum(int number)
+if (amount < 3 || amount > 3)
+{
+    Console.WriteLine("You entered a non-three-digit number");
+}
+else
+int ShowMidNum(int num)
 {
     int remain = number % 100;
     return remain / 10;
