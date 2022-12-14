eb60dcc8c3e7b67453d4653987ec62a02944f097
RedFox <novikov2090@yandex.ru>
2022-12-14 19:35:40 +0300

    lab_34 to lab_38

---
+++ lab_36/Program.cs
@@ -0,0 +1,17 @@
+﻿int[] array = {5, 2, 9, 7, 9, 6, 15, 8, 22, 13, 6, 5};
+
+int OddIndexSum(int[] arr)
+{
+    int sum = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i % 2 != 0)
+        {
+            sum += arr[i];
+        }
+    }
+    return sum;
+}
+
+int oddSum = OddIndexSum(array);
+Console.WriteLine(oddSum);
---
+++ lab_38/Program.cs
@@ -0,0 +1,48 @@
+﻿double[] CreateDoubleArray(int size, int max, int roundTo)
+{
+    Random random = new Random();
+    double[] arr = new double[size];
+    for (int i = 0; i < size; i++)
+    {
+        arr[i] = Math.Round(random.NextDouble()*max, roundTo);
+    }
+    return arr;
+}
+
+double[] MinMax(double[] arr)
+{
+    double[] res = new double[2];
+    
+    res[1] = arr[0]; // max
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if(res[1] > arr[i]) continue;
+        else res[1] = arr[i];
+    }
+    res[0] = arr[0]; // min
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if(res[0] < arr[i]) continue;
+        else res[0] = arr[i];
+    }
+    return res;
+}
+
+void PrintArray(double[] arr)
+{
+    Console.Write("[");
+    for (int i = 0; i < arr.Length - 1; i++)
+    {
+        Console.Write(arr[i] + ", ");
+    }
+    Console.Write(arr[arr.Length - 1]);
+    Console.Write("]");
+}
+
+double[] array = CreateDoubleArray(10, 100, 2);
+PrintArray(array);
+double[] minMax = MinMax(array);
+Console.WriteLine();
+PrintArray(minMax);
+double maxMinDiff = Math.Round(minMax[1] - minMax[0], 2);
+Console.Write(" -> " + maxMinDiff);
